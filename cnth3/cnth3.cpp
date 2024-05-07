43714200e2b53459d15d7d620f7d8baed2d4334d
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-07 11:18:27 +0700

    menambahkan prosedur

--- cnth3/cnth3.cpp
+++ cnth3/cnth3.cpp
@@ -24,6 +24,14 @@ void siswa::displayAll(orang& a) {
     cout << id << endl << a.nama;
 }
 
+void orang::setNama(string pNama) {
+    nama = pNama;
+}
+
+void siswa::setId(int pId) {
+    id = pId;
+}
+
 int main()
 {
     
